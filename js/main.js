aa9e299b717297e55d4af6d72c7578a51470177c
SergiyIlchyshyn <sergiy.ilchyshyn@gmail.com>
2021-01-10 21:48:09 +0200

    updated home page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log('Hello');
+/* header start */
+(function() {
+    let headerNav = document.querySelector('.header__menu');
+    let sticky = headerNav.offsetTop;
+
+    window.addEventListener('scroll', function() {
+        if (window.pageYOffset > sticky) {
+            headerNav.classList.add('is-active');
+        } else {
+            headerNav.classList.remove('is-active');
+        }
+    });
+})();
+/* header end */
